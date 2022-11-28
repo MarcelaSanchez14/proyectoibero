fea61d9b1f4454025bf88f209f1d380c7e6346fd
Marcelasanchez14 <nidiamarcelasanchez@gmail.com>
2022-11-27 22:39:15 -0500

    Cargue inicia de Clases, actividad 1 y actividad 2

---
+++ clase/index.js
@@ -0,0 +1,35 @@
+var n = 100;
+n = 80
+var a = 200;
+var x = a + n
+const prueba = 20
+
+console.log(prueba)
+console.log(x)
+alert('hola mundo llena el formulario')
+
+var nombre = "Marcela"
+var apellido = "Sanchez"
+
+console.log(nombre + " " + apellido)
+
+var sumar = function (){
+    var n1 = 8
+    var n2 = 22
+    console.log(n1+n2) 
+}
+
+sumar()
+
+var restar =function(){
+    var n1 =80
+    var n2 = 20
+    return n1 - n2
+}
+console.log(restar())
+
+var dividir = function (n1, n2){
+    console.log (n1/n2)
+}
+dividir(8,2)
+dividir(20,5)
